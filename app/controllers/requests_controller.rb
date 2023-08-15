f63cce9ec83f2137f14199d0f2685b4bc16e3e43
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-15 16:54:35 +0900

    ルート、フォームの下準備

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -7,4 +7,14 @@ class RequestsController < ApplicationController
     @request = Request.new
   end
 
+  def create
+    Request.create(request_params)
+    redirect_to '/'
+  end
+
+  private
+  def request_params
+    params.require(:request).permit(:title, :date, :dexcription, :expected_length, :expected_place)
+  end
+
 end
