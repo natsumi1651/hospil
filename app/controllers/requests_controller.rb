3d04617810f7e44884a984b883d9f4bdf19dcddb
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-15 19:41:15 +0900

    修正

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,6 @@
 class RequestsController < ApplicationController
   def index
+    @requests = Request.all
   end
 
 
@@ -10,6 +11,11 @@ class RequestsController < ApplicationController
   def create
     @request = Request.create(request_params)
     redirect_to '/'
+  end
+
+  def show
+    # @request = Request.find(params[:id])
+    
   end
 
   private
