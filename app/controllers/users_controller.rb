1353f47adb553a84ac8711cfa5c9bf4c83163264
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-18 13:49:20 +0900

    一通りのcss完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @nickname = current_user.nickname
-    @requests = current_user.requests
+    @requests = current_user.requests.order(created_at: :desc)
   end
 
 end
