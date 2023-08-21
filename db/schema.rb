148c5b8e8a0167ec93e5bc4d464174c0af898156
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-21 17:45:44 +0900

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_052038) do
-  create_table "categories", charset: "utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_050810) do
   create_table "comments", charset: "utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "request_id"
