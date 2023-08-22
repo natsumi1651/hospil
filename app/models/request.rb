a81e1de9c1948aca64590bb2a0e6c47857c31fbd
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-22 17:57:47 +0900

    コメント数の表示

--- app/models/request.rb
+++ app/models/request.rb
@@ -10,7 +10,7 @@ class Request < ApplicationRecord
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :image
 
   def self.search(search)
