83921746ec321b9b2cefa75d0eca7535c613a851
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-24 17:35:34 +0900

    indexに作成した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_060419) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_070935) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_060419) do
     t.integer "user_id"
     t.integer "request_id"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
