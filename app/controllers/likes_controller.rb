92dcce67d53a599eadcffd2f06546ae4d9c49e25
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-28 12:52:58 +0900

    README等編集

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,25 @@
 class LikesController < ApplicationController
+  before_action :set_request
+
   def create
     like = current_user.likes.build(request_id: params[:request_id])
     like.save
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
 
+  
   def destroy
     like = Like.find_by(request_id: params[:request_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def set_request
+    @request = Request.find(params[:request_id])
   end
 
 end
